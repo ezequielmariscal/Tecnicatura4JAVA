c7c1006b7afcfc435978f704edbb6d291cebcfe6
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-08-25 23:34:21 -0300

    Add_class1_part5

--- Leccion01/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
+++ Leccion01/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
@@ -18,13 +18,14 @@ public class SistemaEstudiantesApp {
         */
         var consola = new Scanner(System.in); // Creamos consola, se encarga de leer la informacion
         // Creamos una instancia de la clase servicio, esto lo hacemos fuera del ciclo while
-        var estudianteDao = new EstudianteDAO(); // Esta instancia debe hacerse una vez
+        var estudianteDAO = new EstudianteDAO(); // Esta instancia debe hacerse una vez
         // Empezamos el menu
         var salir = false;
         while(!salir){
             try{
-                mostrarMenu(); // Este sera el metodo que devolvera un booleano
-                salir = ejecutarOpciones(consola, estudianteDao); // Este arroja una exception
+                mostrarMenu(); // Mostramos menu
+                // Este sera el metodo ejecutarOpciones que devolvera un booleano
+                salir = ejecutarOpciones(consola, estudianteDAO); // Este arroja una exception
             } catch (Exception e){
                 System.out.println("Ocurrio un error: "+e.getMessage());
             }
@@ -35,9 +36,38 @@ public class SistemaEstudiantesApp {
     private static void mostrarMenu(){
         System.out.println("""
                 ******* Sistema de Estudiantes *******
-                
-                """
-
-        );
+                1. Listar estudiantes
+                2. Buscar estudiante
+                3. Agregar estudiante
+                4. Modificar estudiante
+                5. Eliminar estudiante
+                6. Salir
+                Elige una opción:
+                """);
     }
+    // Metodo para ejecutar las opciones, va aregresar un valor booleano, ya que es el que
+    //puede modificar el valor de la variable salir, si es verdadero termina el ciclo while
+    private static boolean ejecutarOpciones(Scanner consola, EstudianteDAO estudianteDAO){
+        var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        // Revisamos la opcion digita a traves de un switch
+        switch (opcion){
+            case 1 -> { //Listar estudiantes
+                System.out.println("Listado de estudiantes... ");
+                // no muestra la informacion, solo recupera la info y regresa una lista
+
+            } // Fin caso 1
+            case  2 -> { // Listar a las personas
+                System.out.println("Listado de personas: ");
+                // Mejoras con lambda y el metodo de referencia
+                // personas.forEach((persona) -> System.out.println(persona));
+                personas.forEach(System.out::println); // :: se conoce como metodo de referencia
+            } // Fin caso 2
+            case 3 -> { // Salir del ciclo
+                System.out.println("Hasta pronto...");
+                salir = true;
+            } // Fin del caso 3
+            default -> System.out.println("Opcion incorrecta: "+opcion);
+        } // Fin del switch
+        return salir;
 }// Fin clase

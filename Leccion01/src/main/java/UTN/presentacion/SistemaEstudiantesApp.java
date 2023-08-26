841c23e643dcb1fae1e6fe7b2a1c1d14b639b692
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-08-25 23:55:51 -0300

    Add_class1_part6

--- Leccion01/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
+++ Leccion01/src/main/java/UTN/presentacion/SistemaEstudiantesApp.java
@@ -1,6 +1,7 @@
 package UTN.presentacion;
 
 import UTN.conexion.Conexion;
+import UTN.dominio.Estudiante;
 import UTN.servicio.EstudianteDAO;
 import com.mysql.cj.x.protobuf.MysqlxDatatypes;
 
@@ -55,19 +56,79 @@ public class SistemaEstudiantesApp {
             case 1 -> { //Listar estudiantes
                 System.out.println("Listado de estudiantes... ");
                 // no muestra la informacion, solo recupera la info y regresa una lista
-
+                var estudiantes = estudianteDAO.listarEstudiantes(); // recibe el listado
+                // vamos a iterar cada objeto de tipo estudiante
+                estudiantes.forEach(System.out::println); // para imprimir lista¿
             } // Fin caso 1
-            case  2 -> { // Listar a las personas
-                System.out.println("Listado de personas: ");
-                // Mejoras con lambda y el metodo de referencia
-                // personas.forEach((persona) -> System.out.println(persona));
-                personas.forEach(System.out::println); // :: se conoce como metodo de referencia
+            case  2 -> { // Buscar estudiante por id
+                System.out.println("Introduce el id_estudiante a buscar: ");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                var estudiante = new Estudiante(idEstudiante);
+                var encontrado = estudianteDAO.buscarEstudiantePorId(estudiante);
+                if(encontrado)
+                    System.out.println("Estudiante encontrado: "+estudiante);
+                else
+                    System.out.println("Estudiante no encontrado: "+estudiante);
             } // Fin caso 2
-            case 3 -> { // Salir del ciclo
-                System.out.println("Hasta pronto...");
-                salir = true;
+            case 3 -> { // Agregar estudiante
+                System.out.print("Agregar estudiante: ");
+                System.out.print("Digite el nombre: ");
+                var nombre = consola.nextLine();
+                System.out.print("Digite el apellido: ");
+                var apellido = consola.nextLine();
+                System.out.print("Digite el telefono: ");
+                var telefono = consola.nextLine();
+                System.out.print("Digite el email: ");
+                var email = consola.nextLine();
+                // creamos el objeto estudiante sin id
+                var estudiante = new Estudiante(nombre, apellido, telefono, email);
+                // Agregamos la persona a la lista
+                var agregado = estudianteDAO.agregarEstudiante(estudiante);
+                if(agregado)
+                    System.out.println("Estudiante agregado: "+estudiante);
+                else
+                    System.out.println("Estudiante NO agregado: "+estudiante);
             } // Fin del caso 3
-            default -> System.out.println("Opcion incorrecta: "+opcion);
+            case 4 -> {
+                System.out.println("Modificar Estudiante: ");
+                System.out.print("Id Estudiante: ");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                System.out.print("Nombre: ");
+                var nombre = consola.nextLine();
+                System.out.print("Apellido: ");
+                var apellido = consola.nextLine();
+                System.out.print("Telefono: ");
+                var telefono = consola.nextLine();
+                System.out.print("Email: ");
+                var email = consola.nextLine();
+
+                var estudiante =
+                        new Estudiante(idEstudiante, nombre, apellido, telefono, email);
+                var modificado = estudianteDAO.modificarEstudiante(estudiante);
+                if (modificado)
+                    System.out.println("Estudiante modificado: " + estudiante);
+                else
+                    System.out.println("Estudiante no modificado: " + estudiante);
+            }
+            case 5 -> {
+                System.out.println("Eliminar Estudiante: ");
+                System.out.print("Id estudiante: ");
+                var idEstudiante = Integer.parseInt(consola.nextLine());
+                var estudiante = new Estudiante(idEstudiante);
+                var eliminado = estudianteDAO.eliminarEstudiante(estudiante);
+                if(eliminado)
+                    System.out.println("Estudiante  eliminado: " + estudiante);
+                else
+                    System.out.println("Estudiante no eliminado" + estudiante);
+
+            }
+            case 6 -> {
+                System.out.println("Finalizo el programa");
+                salir = true;
+            }
+            default -> System.out.println("Opción no reconocida, ingrese otra opción");
+
         } // Fin del switch
         return salir;
+    }
 }// Fin clase

606accac63b43c1d01afae906036974db19c0171
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-10-05 00:41:23 -0300

    Add_class3_par2

--- Leccion02/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
+++ Leccion02/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -1,4 +1,13 @@
 package utn.estudiantes.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity // Identifica una clase de identidad que representa una tabla de datos
 public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // nuestra llave es autoincrementable en nuestra bd
+    private Integer idEstudiante;
 }

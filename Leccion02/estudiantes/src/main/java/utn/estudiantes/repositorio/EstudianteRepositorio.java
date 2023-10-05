52cc29e5ad173100dcf02249aeb9fb7304ef86f2
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-10-05 20:45:05 -0300

    Add_class3_part5,6,7_finished

--- Leccion02/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
+++ Leccion02/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
@@ -3,5 +3,6 @@ package utn.estudiantes.repositorio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import utn.estudiantes.modelo.Estudiante;
 
+// Esta es una clase interface del tipo repository antes se le agregaba la conotacion @repository ahora yano hace falta
 public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
 }
---
+++ Leccion02/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -0,0 +1,13 @@
+package utn.estudiantes.servicio;
+
+
+import utn.estudiantes.modelo.Estudiante;
+
+import java.util.List;
+
+public class EstudianteServicio implements IEstudianteServicio{
+    public List<Estudiante> listarEstudiantes();
+    public Estudiante buscarEstudiantePorId(Integer estudiante);
+    public void guardarEstudiante(Estudiante estudiante);
+    public void eliminarEstudiante(Estudiante estudiante);
+}

04417d58351f1c034b0a7cf787473654cc04027a
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-10-05 21:53:10 -0300

    Add_class4_part5

--- Leccion02/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Leccion02/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,12 +1,24 @@
 package utn.estudiantes;
 
+import org.slf4j.Logger; // es una interface
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
 
 // implementamos commandlinerinner para levantar nuestra fabrica de spring mediante el method run
 @SpringBootApplication
 public class EstudiantesApplication implements CommandLineRunner {
+
+	@Autowired // xq queremos utilizar la capa de servicio en esta aplicacion, al ejecutar el metodo main se hacela ejecucion de manera automatica
+	private EstudianteServicio estudianteServicio;
+	private static final
+	Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class); // utilizamos loggerfactory, para obtener un logger y obtener la info de consola
+	// el patron esta en el logback
+
+
 
 	public static void main(String[] args) {
 

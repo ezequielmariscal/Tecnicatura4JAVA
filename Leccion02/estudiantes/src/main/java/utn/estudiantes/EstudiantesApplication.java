46322c4eb00fc7ab96720b4d9848409675bd7f9f
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-10-05 21:46:50 -0300

    Add_class4_part4

--- Leccion02/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Leccion02/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,20 @@
 package utn.estudiantes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// implementamos commandlinerinner para levantar nuestra fabrica de spring mediante el method run
 @SpringBootApplication
-public class EstudiantesApplication {
+public class EstudiantesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EstudiantesApplication.class, args);
 	}
 
+	@Override // no es static es un metodo normal
+	public void run(String... args) throws Exception {
+
+	}
 }

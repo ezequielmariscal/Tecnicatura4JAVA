cdebb579b87d4f7dd9a93a0eeaa675b177b14ec0
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-10-05 21:29:57 -0300

    Add_class4_part1

--- Leccion02/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Leccion02/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,13 +1,38 @@
 package utn.estudiantes.servicio;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
 
 import java.util.List;
 
+@Service
 public class EstudianteServicio implements IEstudianteServicio{
-    public List<Estudiante> listarEstudiantes();
-    public Estudiante buscarEstudiantePorId(Integer estudiante);
-    public void guardarEstudiante(Estudiante estudiante);
-    public void eliminarEstudiante(Estudiante estudiante);
+
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer estudiante) {
+        return null;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+
+    }
+
+
 }
--- Leccion02/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++ Leccion02/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -1,4 +1,15 @@
 package utn.estudiantes.servicio;
 
+import utn.estudiantes.modelo.Estudiante;
+
+import java.util.List;
+
 public interface IEstudianteServicio {
+    List<Estudiante> listarEstudiantes();
+
+    Estudiante buscarEstudiantePorId(Integer estudiante);
+
+    void guardarEstudiante(Estudiante estudiante);
+
+    void eliminarEstudiante(Estudiante estudiante);
 }
